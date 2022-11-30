6865acb2f175f75228be6903a61649845bb705ef
João Victor Carvalho <joaocarvoli@pop-os.localdomain>
2022-11-29 21:38:58 -0300

    Creating NavBar component

---
+++ frontendVue/src/components/NavBar/DropList.vue
@@ -0,0 +1,13 @@
+<template>
+    <li class="products nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Produtos
+        </a>
+        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Óculos De Grau</a></li>
+            <li><a class="dropdown-item" href="#">Óculos De Sol</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Sobre</a></li>
+        </ul>
+    </li>
+</template>
---
+++ frontendVue/src/components/NavBar/SearchBar.vue
@@ -0,0 +1,6 @@
+<template>
+  <form class="d-flex align-items-end">
+    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+    <button class="btn btn-outline-success align-content-end" type="submit">Search</button>
+  </form>
+</template>
