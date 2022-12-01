375a96800dc6cec048869f6253b6b8dba8617846
João Victor Carvalho <joaocarvoli@pop-os.localdomain>
2022-11-30 21:49:52 -0300

    Starting to build Profile

---
+++ frontendVue/src/views/Profile.vue
@@ -0,0 +1,8 @@
+<script>
+import NameForms from '../components/Profile/NameForms.vue';
+import PhoneForms from '../components/Profile/PhoneForms.vue';
+import EmailForms from '../components/Profile/EmailForms.vue';
+import Location from '../components/Profile/Location.vue';
+import Password from '../components/Profile/Password.vue';
+</script>
+
